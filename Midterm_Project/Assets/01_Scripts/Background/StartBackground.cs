e855d41e29b589aa147e2b0381a890b2bda5f3bd
js-harang <harang3427@naver.com>
2024-04-16 17:40:06 +0900

    2024-04-16

--- Midterm_Project/Assets/01_Scripts/Background/StartBackground.cs
+++ Midterm_Project/Assets/01_Scripts/Background/StartBackground.cs
@@ -1,24 +1,73 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Background 무한루프
-/// </summary>
 public class StartBackground : MonoBehaviour
 {
-    private GameManager gm;
+    [SerializeField] GameObject background;
+    private List<GameObject> currentBackground;
 
-    [SerializeField] private int startPosition;
+    private float screenWidth;
 
     private void Start()
     {
-        gm = GameManager.instance;
+        currentBackground = new List<GameObject>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            Vector2 position = new Vector2(10 * i, 0);
+            GameObject obj = Instantiate(background, position, Quaternion.identity);
+            currentBackground.Add(obj);
+        }
+
+        //screenWidth = currentBackground[0].transform.;
     }
 
     private void Update()
     {
-        transform.position = new Vector3(10 - ((Time.time + startPosition) % 20), 1, 0);
-
-        if (transform.position.x <= -10)
-            transform.position = new Vector3(10, 1, 0);
+        BackgroundPosition();
     }
+
+    private void BackgroundPosition()
+    {
+        GameObject obj = currentBackground[0];
+        float moveStageX = Camera.main.transform.position.x + screenWidth;
+        float startX = currentBackground[currentBackground.Count - 1].transform.position.x + screenWidth;
+
+        if (currentBackground[currentBackground.Count - 1].transform.position.x < moveStageX)
+        {
+            obj.transform.position = new Vector2(startX, 0);
+            currentBackground.Remove(obj);
+            currentBackground.Add(obj);
+        }
+    }
+
+    private void MoveBackground()
+    {
+
+    }
+
+    //[SerializeField] GameObject background;
+    //[SerializeField] List<GameObject> currentBackground;
+
+    //private void Update()
+    //{
+    //    create
+    //}
+
+    //private GameManager gm;
+
+    //[SerializeField] private int startPosition;
+
+    //private void Start()
+    //{
+    //    gm = GameManager.instance;
+    //}
+
+    //private void Update()
+    //{
+    //    transform.position = new Vector3(10 - ((Time.time + startPosition) % 20), 1, 0);
+
+    //    if (transform.position.x <= -10)
+    //        transform.position = new Vector3(10, 1, 0);
+    //}
 }
